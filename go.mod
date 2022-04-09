ff45da037154185916fa6f0d83d5d0b49f3bee55
Jimmy Samanda Rasu <jimmy.rasu@pegadaian.co.id>
2022-04-10 05:11:22 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module repo.pegadaian.co.id/ms-pds/modules/logger
+
+go 1.17
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
---
+++ logger.go
@@ -0,0 +1,131 @@
+package logger
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	timestampFormat   = "2006-01-02 15:04:05.000"
+	starString        = "**********"
+	fieldKeyRequestId = "_requestId"
+	fieldKeyData      = "data"
+)
+
+var (
+	strExclude = []string{"password", "base64", "npwp", "phone", "nik", "ktp", "gaji", "othr",
+		"slik"}
+
+	requestId = ""
+)
+
+type loglevel string
+
+func NewLogger(reqId string) {
+	requestId = reqId
+}
+
+func Init(loglvl loglevel) {
+	logrus.SetReportCaller(true)
+	formatter := &logrus.JSONFormatter{
+		TimestampFormat: timestampFormat,
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			tmp := strings.Split(f.File, "/")
+			filename := tmp[len(tmp)-1]
+			return "", fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyMsg:  "_message",
+			logrus.FieldKeyTime: "_time",
+		},
+	}
+
+	logrus.SetFormatter(formatter)
+	// default log level
+	logLevel := logrus.DebugLevel
+	logrus.SetLevel(logLevel)
+	logLevel, err := logrus.ParseLevel(string(loglvl))
+
+	if err != nil {
+		Make().Debug(err)
+	}
+
+	logrus.SetLevel(logLevel)
+}
+
+func Make(data ...map[string]interface{}) *logrus.Entry {
+	dataMap := map[string]interface{}{}
+	logField := logrus.Fields{}
+	logField[fieldKeyRequestId] = requestId
+	logrus.SetReportCaller(true)
+
+	if len(data) > 0 {
+		dataMap = data[0]
+		payloadExcluder(&dataMap)
+		logField[fieldKeyData] = dataMap
+	}
+
+	return logrus.WithFields(logField)
+}
+
+func MakeWithoutReportCaller(data ...map[string]interface{}) *logrus.Entry {
+	log := Make(data...)
+	logrus.SetReportCaller(false)
+
+	return log
+}
+
+func Dump(strct ...interface{}) {
+	fmt.Println("DEBUGGING ONLY")
+	spew.Dump(strct)
+	fmt.Println("DEBUGGING ONLY")
+}
+
+func reExcludePayload(pl interface{}) (map[string]interface{}, bool) {
+	vMap, ok := pl.(map[string]interface{})
+
+	if !ok {
+		return map[string]interface{}{}, ok
+	}
+
+	payloadExcluder(&vMap)
+
+	return vMap, true
+}
+
+func payloadExcluder(pl *map[string]interface{}) {
+	var ok bool
+	var vMap map[string]interface{}
+	plMap := *pl
+
+	for k, v := range plMap {
+		vMap, ok = reExcludePayload(v)
+
+		if ok {
+			plMap[k] = vMap
+			continue
+		}
+
+		if contains(strExclude, k) {
+			v = starString
+		}
+
+		plMap[k] = v
+	}
+
+	*pl = plMap
+}
+
+func contains(strIncluder []string, str string) bool {
+	for _, include := range strIncluder {
+		if strings.Contains(strings.ToLower(str), include) {
+			return true
+		}
+	}
+
+	return false
+}
